b69754e4df198e1bbdf18a3673d745d0748db5ed
Just_Val <185958622+JustVal-su@users.noreply.github.com>
2026-05-18 21:47:35 +0200

    fix: added impair line support for images. It was hard.

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,9 @@ use serde::Deserialize;
 //make code cleaner
 //make possible to import any compatible json file with a cli argument
 //make a tiny pixel art editor to stop having to place all the pixels manually
-
+//display warning when specified height isn't equal to the true height
+//fix bug : when a height is specified (and is odd), if there is another line in the file, it is displayed where it shouldn't be
+//handle "thread 'main' panicked at src/main.rs:28:26:  index out of bounds: the len is 17 but the index is 17  note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace   when specified height isn't equal to real one"
 
 // Importing the object that will be used to display the image
 #[derive(Debug, Deserialize)]
@@ -37,20 +39,51 @@ fn check(image: &Vec<Vec<[u8; 3]>>, height: u32, width: u32) -> bool {
     true
 }
 
+fn is_odd(n: u32) -> bool {
+    n % 2 != 0
+}
+
 fn display(parsed_file: Image) {
-    let image = (0..parsed_file.height).step_by(2); //change that 
+    let image; //change name
+    if is_odd(parsed_file.height) == true {
+        image = (0..(parsed_file.height -1)).step_by(2);
+    } else {
+        image = (0..parsed_file.height).step_by(2);
+    }
+     
+    /*if is_odd(parsed_file.height) == true {
+        image = (0..(parsed_file.height - 1)).step_by(2)
+    } else {
+        
+    }*/
+
     for i in image {
         let line = &parsed_file.image[i as usize];
         let second_line = &parsed_file.image[(i + 1) as usize];
         for j in 0..line.len() {
+
             let pixel_one = &line[j as usize];
             let pixel_two = &second_line[j as usize];
+            
             print!(
                 "{}",
                 "▀".truecolor(pixel_one[0], pixel_one[1], pixel_one[2]).on_truecolor(pixel_two[0], pixel_two[1], pixel_two[2])
             );
         }
         print!("\n");  
+    }
+
+    if is_odd(parsed_file.height) == true {
+        let last_index = parsed_file.height - 1;
+        let line_last = &parsed_file.image[last_index as usize];
+        for j in 0..line_last.len() {
+            let pixel = &line_last[j as usize];
+            print!(
+                "{}",
+                "▀".truecolor(pixel[0], pixel[1], pixel[2])
+            );
+        }
+                
     }
 }
 
